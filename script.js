008617a472f64ee9605182ac07ae2b4dc506c0c3
Abdul-Raheem <shafiq.abdulraheem1@gmail.com>
2023-05-28 13:12:01 +0500

    Added functionality to randomly create a message

--- script.js
+++ script.js
@@ -2,5 +2,15 @@
 
 const placesList = ['Paris','Rome','London','Istanbul','Melbourne','Cairo','Rio','Islamabad'];
 const namesList = ['Bob','Jim','Jeff','John','Tom','Alex','David'];
-const verbsList = ['did nothing','went swimming','watched a football match','got arrested','had dinner'];
+const verbsList = ['did nothing','went swimming','watched a football match','had dinner'];
 
+function randomElement(arr){
+    // returns random element in array
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function CreateMessage(){
+    return `I went to ${randomElement(placesList)} with ${randomElement(namesList)} and ${randomElement(verbsList)}.`
+}
+
+console.log(CreateMessage());
