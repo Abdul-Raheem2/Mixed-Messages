1622e1629ccaf72ae55e36aa583435d6e747419f
Abdul-Raheem <shafiq.abdulraheem1@gmail.com>
2023-05-28 13:00:57 +0500

    Added the store message components

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('test');
+//console.log('test');
+
+const placesList = ['Paris','Rome','London','Istanbul','Melbourne','Cairo','Rio','Islamabad'];
+const namesList = ['Bob','Jim','Jeff','John','Tom','Alex','David'];
+const verbsList = ['did nothing','went swimming','watched a football match','got arrested','had dinner'];
+
